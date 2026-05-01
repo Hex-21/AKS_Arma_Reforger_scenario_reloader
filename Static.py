14a4020484b5c21c19842116c7010a3a4f781332
Hex <43280570+Hex-21@users.noreply.github.com>
2026-05-01 19:24:52 +0200

    Update Static.py
    Rewrite

--- Static.py
+++ Static.py
@@ -1,29 +1,54 @@
-# Loading scenarios from top to bottom(ScenarioConfig)
 import json
 import logging
-import random
-import time
+from datetime import datetime
+from os import path
+from time import sleep
 
-ConfigPathArma = "/pathtoconfigfile.conf"
-ScenarioConfig = "./ScenarioConfig.json"
-
+try:
+    if path.getsize("PathToLogFile/Log.log") > 50000000:
+        with open("PathToLogFile/Log.log", "w", encoding="utf-8") as f:
+            f.write('')
+except FileNotFoundError:
+    pass
 logging.basicConfig(
-    filename="Log.log",
+    filename="PathToLogFile/Log.log",
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.DEBUG
+    level=logging.DEBUG,
 )
 
+ConfigPathArma = "/PathToServerConfig/config.json"
+ScenarioConfig = "/PathToScenarioConfig/WeekScenarioConfig.json"
+logging.info(f"Config path: {ConfigPathArma} | Scenario path: {ScenarioConfig}")
+
 while True:
-    with open(f"{ScenarioConfig}", "r") as f:
+    with open(f"{ConfigPathArma}", "r", encoding="utf-8") as f:
         config = json.load(f)
-        maps = config['scenarios']
+    try:
+        with open(ScenarioConfig, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            logging.debug(f"Read {ScenarioConfig} with the scenarios: {data}")
+    except Exception as e:
+        logging.error(f"Failed to read {ScenarioConfig} with the Exception: {e}")
 
-    for map in maps:
-        with open(f"{ConfigPathArma}", "r", encoding="utf-8") as config_file:
-            config = json.load(config_file)
-            config['game']['scenarioId'] = f"{map}"
 
-        with open(f"{ConfigPathArma}", "w", encoding="utf-8") as f:
-            json.dump(config, f, indent=4)
-            logging.info(f"Scenario '{map}' loaded in config")
-        time.sleep(60 * 60 * 24 * 1)  # 48H (60*60*24*1) #60(Sekunden)*60(Minuten)*24(Stunden)*(Tage)
+    scenarios = data["scenarios"]
+    tday = datetime.today().strftime("%A").lower()
+    scenario = scenarios.get(tday)
+    logging.info(f"Scenario: {scenario} for today: {tday}")
+
+    try:
+        with open(ConfigPathArma, "r", encoding="utf-8") as f:
+            arma_config_scenario = json.load(f)
+            logging.debug(f"Content of {ConfigPathArma}: {arma_config_scenario}")
+        arma_config_scenario["game"]["scenarioId"] = scenario
+        logging.debug(f"Read {ConfigPathArma} with the previous scenario: {arma_config_scenario}")
+    except Exception as e:
+        logging.error(f"Failed to read {ConfigPathArma} with the Exception: {e}")
+
+    with open(ConfigPathArma, "w", encoding="utf-8") as f:
+        json.dump(arma_config_scenario, f, indent=4)
+        logging.debug(f"Overwriting {ConfigPathArma} with the content: {arma_config_scenario}")
+
+    logging.debug(f"Scenario: {scenario} for today: {tday}, now sleeping 1Hour")
+    sleep(60 * 60 * 1)
+exit(1)
